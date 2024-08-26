7e7158253844d563715524ef088e8618e21bfcbe
TichaTin <ticha@arlula.com>
2024-08-26 22:06:22 +1000

    updated srcSet

--- src/utils/Pictures.jsx
+++ src/utils/Pictures.jsx
@@ -5,10 +5,10 @@ const Pictures = ({ s_url, m_url, l_url, alt_text, parent_classes}) => {
         <picture className={parent_classes}>
 
             {m_url && (
-                <source media="(min-width:768px)" srcset={m_url} />
+                <source media="(min-width:768px)" srcSet={m_url} />
             )}
             {l_url && (
-                <source media="(min-width:1024px)" srcset={l_url} />
+                <source media="(min-width:1024px)" srcSet={l_url} />
             )}
             {(s_url) && (
                 <img src={s_url} alt={alt_text}/>
