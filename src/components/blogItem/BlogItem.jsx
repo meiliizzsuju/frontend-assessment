93e1f5ba4c28b18567f8618b206a7cfed2155af4
TichaTin <ticha@arlula.com>
2024-08-26 21:34:41 +1000

    update blog layout

--- src/components/blogItem/BlogItem.jsx
+++ src/components/blogItem/BlogItem.jsx
@@ -4,13 +4,17 @@ import './BlogItem.css';
 
 const BlogItem = ({ s_url, m_url, l_url, alt_text, parent_classes, content, link_url, link_classes, link_label }) => {
     return (
-        <div className='blogitem p-4 pb-6 rounded border-2 border-gray-200 shadow-md text-center'>
-            <Pictures s_url={s_url} m_url={m_url} l_url={l_url} alt_text={alt_text} parent_classes={parent_classes} />
-            
-            {content && (
-                <p className='mt-2 mb-4'>{content}</p>
-            )}
-            <a href={link_url} className={link_classes}>{link_label}</a>
+        <div className={'blogitem p-4 pb-6 rounded border-2 border-gray-200 shadow-md text-center flex flex-col justify-between'}>
+            <div className='blogitem__top'>
+                <Pictures s_url={s_url} m_url={m_url} l_url={l_url} alt_text={alt_text} parent_classes={parent_classes} />
+
+                {content && (
+                    <p className='mt-2 mb-4'>{content}</p>
+                )}
+            </div>
+            <div className='blogitem__bottom'>
+                <a href={link_url} className={link_classes}>{link_label}</a>
+            </div>
         </div>
     )
 }
--- src/components/blogs/Blogs.jsx
+++ src/components/blogs/Blogs.jsx
@@ -4,12 +4,28 @@ import BlogItem from '../blogItem/BlogItem'
 const Blogs = () => {
     return (
         <section className='blog p-4 md:p-8'>
-            <div className='container mx-auto'>
+            <div className='container mx-auto grid grid-cols-1 md:grid-cols-3 gap-10'>
                 <BlogItem s_url={'https://via.placeholder.com/400x300'}
                     alt_text="blog image 1"
                     parent_classes={'blogitem__img'}
                     content={'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do elusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'}
                     link_url='#blog1'
+                    link_classes='text-sm font-bold tracking-wider uppercase rounded-full border-solid border-2 border-gray-800 px-3 py-2 mt-2'
+                    link_label='Read more'
+                />
+                <BlogItem s_url={'https://via.placeholder.com/400x300'}
+                    alt_text="blog image 2"
+                    parent_classes={'blogitem__img'}
+                    content={'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eusmod tempor incididunt ut labore et dolore magna aliqua.'}
+                    link_url='#blog2'
+                    link_classes='text-sm font-bold tracking-wider uppercase rounded-full border-solid border-2 border-gray-800 px-3 py-2 mt-2'
+                    link_label='Read more'
+                />
+                <BlogItem s_url={'https://via.placeholder.com/400x300'}
+                    alt_text="blog image 3"
+                    parent_classes={'blogitem__img'}
+                    content={'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.'}
+                    link_url='#blog3'
                     link_classes='text-sm font-bold tracking-wider uppercase rounded-full border-solid border-2 border-gray-800 px-3 py-2 mt-2'
                     link_label='Read more'
                 />
